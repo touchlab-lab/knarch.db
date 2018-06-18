3bbe9e5d939620f89dd0672bc6d3bba919490202
Kevin Galligan <kgalligan@gmail.com>
2018-06-18 11:34:04 -0400

    Licenses sorted

--- kotlin/knarch-ios/src/main/kotlin/co/touchlab/knarch/db/sqlite/SQLiteStatementInfo.kt
+++ kotlin/knarch-ios/src/main/kotlin/co/touchlab/knarch/db/sqlite/SQLiteStatementInfo.kt
@@ -1,6 +1,9 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
  *
+ * CHANGE NOTICE: File modified by Touchlab Inc to port to Kotlin and generally prepare for Kotlin/Native
+ *
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
